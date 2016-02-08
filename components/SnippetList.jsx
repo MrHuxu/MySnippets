c3cb708e76f14192b737e7d473c86e862e26624e
MrHuxu <hxtheone@gmail.com>
2016-02-08 18:39:08 +0800

    make the selectable list work

--- components/SnippetList.jsx
+++ components/SnippetList.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import List from 'material-ui/lib/lists/list';
+import {SelectableContainerEnhance} from 'material-ui/lib/hoc/selectable-enhance';
+const SelectableList = SelectableContainerEnhance(List);
 import ListItem from 'material-ui/lib/lists/list-item';
 import ActionGrade from 'material-ui/lib/svg-icons/action/grade';
 import ActionInfo from 'material-ui/lib/svg-icons/action/info';
@@ -9,24 +11,49 @@ import ContentDrafts from 'material-ui/lib/svg-icons/content/drafts';
 import ContentSend from 'material-ui/lib/svg-icons/content/send';
 import Divider from 'material-ui/lib/divider';
 import SnippetItem from './SnippetItem';
-import ListExampleSelectable from './test';
 
 class SnippetList extends List {
+  constructor (props, context) {
+    super(props, context);
+    this.state = {
+      selectedIndex: 0
+    };
+
+    this.handleUpdateSelectedIndex = this.handleUpdateSelectedIndex.bind(this);
+  }
+
+  handleUpdateSelectedIndex (e, index) {
+    this.setState({
+      selectedIndex: index,
+    });
+  }
+
   render () {
     const { records } = this.props;
-    const items = records.map(record => <ListItem key={record._id}><SnippetItem metaData={record} /></ListItem>);
+    const items = records.map((record, index) => <ListItem value={index} key={record._id}><SnippetItem metaData={record} /></ListItem>);
 
     return (
       <div>
-        {ListExampleSelectable()}
+        <SelectableList
+          valueLink={{
+            value: this.state.selectedIndex,
+            requestChange: this.handleUpdateSelectedIndex
+          }}
+        >
+          {items}
+        </SelectableList>
       </div>
     );
   }
 }
 
+SnippetList.contextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
 var mapStateToProps = function (state) {
   return {
-    records: Object.keys(state.snippet.records).map(key => state.snippet.records[key])
+    records: state.snippet.ids.map(id => state.snippet.entities[id])
   };
 };
 
--- reducers/SnippetReducer.js
+++ reducers/SnippetReducer.js
@@ -4,22 +4,25 @@ import {
 } from '../actions/SnippetActions';
 
 export function snippet (state = {
-  records: {},
-  snack: {
-    open: false,
-    message: ''
+  ids      : [],
+  entities : {},
+  snack    : {
+    open    : false,
+    message : ''
   }
 }, action) {
   var copy = Object.assign({}, state);
 
   switch (action.type) {
     case ADD_SNIPPET:
-      copy.records[action.content._id] = action.content;
+      copy.ids.unshift(action.content._id);
+      copy.entities[action.content._id] = action.content;
       return copy;
 
     case DELETE_SNIPPET:
-      let message = copy.records[action.content].title;
-      delete copy.records[action.content];
+      copy.ids = copy.ids.filter(id => id !== action.content)
+      let message = copy.entities[action.content].title;
+      delete copy.entities[action.content];
       return Object.assign(copy, {
         snack: {
           open: true,
