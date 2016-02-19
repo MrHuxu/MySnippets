f7dc60245118cd57fef22b61f957aa8f957c5c51
Xu Hu <hxtheone@gmail.com>
2016-02-20 00:14:45 +0800

    use webpack-dev-server instead of express but still not work well

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 const path = require('path');
-const express = require('express');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
-const app = express();
-const compiler = webpack(config);
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'vendor', 'dist')));
-
-app.get('*', (req, res) => {
-  res.render(path.join(__dirname, 'index.ejs'));
-});
-
 const port = 15106;
-app.listen(port, () => console.log('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port));
-
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
@@ -27,5 +13,5 @@ new WebpackDevServer(webpack(config), {
     console.log(err);
   }
 
-  console.log('Listening at localhost:%s', port);
+  console.log('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 });
