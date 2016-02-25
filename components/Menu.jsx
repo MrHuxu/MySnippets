f911a84f0f073b6f588dd47e0b7811d8269a5be2
Xu Hu <hxtheone@gmail.com>
2016-02-25 23:57:31 +0800

    add the import/export dialog

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -10,6 +10,8 @@ import Colors from 'material-ui/lib/styles/colors';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import VerticalAlignCenter from 'material-ui/lib/svg-icons/editor/vertical-align-center';
 import TextField from 'material-ui/lib/text-field';
+import FlatButton from 'material-ui/lib/flat-button';
+import Dialog from 'material-ui/lib/dialog';
 import { createSnippet, fetchSnippets } from '../actions/SnippetActions';
 
 const style = {
@@ -32,14 +34,25 @@ class Menu extends Component {
   constructor (props, context) {
     super(props, context);
     this.state = {
-      condition: ''
+      condition        : '',
+      openExportDialog : false
     };
 
     this.handleClickAddButton = this.handleClickAddButton.bind(this);
+    this.handleClickExportButton = this.handleClickExportButton.bind(this);
+    this.closeExportDialog = this.closeExportDialog.bind(this);
   }
 
   handleClickAddButton () {
     this.props.dispatch(createSnippet());
+  }
+
+  handleClickExportButton () {
+    this.setState({ openExportDialog: true });
+  }
+
+  closeExportDialog () {
+    this.setState({ openExportDialog: false });
   }
 
   shouldComponentUpdate (props, state) {
@@ -50,6 +63,18 @@ class Menu extends Component {
   }
 
   render () {
+    const actions = [
+      <FlatButton
+        label      = "Import"
+        secondary  = {true}
+        onTouchTap = {this.closeExportDialog}
+      />,
+      <FlatButton
+        label      = "Export"
+        primary    = {true}
+        onTouchTap = {this.closeExportDialog}
+      />,
+    ];
     return (
       <Toolbar style={{backgroundColor: Colors.cyan500}}>
         <ToolbarGroup firstChild={true} float='left'>
@@ -61,7 +86,16 @@ class Menu extends Component {
         </ToolbarGroup>
 
         <ToolbarGroup lastChild={true} style={style.exportBtn} float='right'>
-          <IconButton><VerticalAlignCenter color={Colors.deepOrange800} /></IconButton>
+          <IconButton onClick={this.handleClickExportButton}><VerticalAlignCenter color={Colors.deepOrange800} /></IconButton>
+          <Dialog
+            title          = 'Import or Export'
+            modal          = {false}
+            actions        = {actions}
+            open           = {this.state.openExportDialog}
+            onRequestClose = {this.closeExportDialog}
+          >
+            Import data from .json file or export data to json.
+          </Dialog>
         </ToolbarGroup>
 
         <ToolbarGroup float='right'>
